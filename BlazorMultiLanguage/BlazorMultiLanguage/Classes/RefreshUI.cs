66a01c08863ec43c5edd74c3b1a5d64e81e64317
Omar Wasfi <alprincewasfi@gmail.com>
2020-06-18 05:09:03 +0200

    Best MultiLanguage in blazor
    This implementation allows me to have different designs when i change the language.
    
    Refrances : https://akmultilanguages.azurewebsites.net/
    https://github.com/aksoftware98/multilanguages

---
+++ BlazorMultiLanguage/BlazorMultiLanguage/Classes/RefreshUI.cs
@@ -0,0 +1,18 @@
+﻿using BlazorMultiLanguage.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMultiLanguage.Classes
+{
+    public class RefreshUI : IRefreshUI
+    {
+        public event Action RefreshRequested;
+
+        public void CallRequestRefresh()
+        {
+            RefreshRequested?.Invoke();
+        }
+    }
+}
---
+++ BlazorMultiLanguage/BlazorMultiLanguage/Services/IRefreshUI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMultiLanguage.Services
+{
+    public interface IRefreshUI
+    {
+        public event Action RefreshRequested;
+        public void CallRequestRefresh();
+    }
+}
